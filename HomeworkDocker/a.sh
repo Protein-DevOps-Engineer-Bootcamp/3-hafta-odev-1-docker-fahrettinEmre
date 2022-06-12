3ea87594cd4be16d9d1532005791218b50a2e3bd
fahrettinEmre <fahrettinemredalga@gmail.com>
2022-06-12 15:06:25 +0000

    done

--- HomeworkDocker/a.sh
+++ HomeworkDocker/a.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
+######################
+#Author : Fahrettin Emre Dalga
+#Date : 27.05.2022
+#Version:5.0.17
+#Usage : Send Mail
+######################
+
+# -r dockerhub'a register etmek  için öncelikle docker login yapılmalıdır.
+
 USAGE_MSG="
 Usage: $(basename $0) [OPTION] <ARGUMENT> ...
 OPTIONS:    ARGUMENTS:         DESCRIPTION:                             DEFAULT VALUE:
-[-m]        <mode>      Branch name to get the build from.       Current Branch
-[-n]        <name>       Compress format of the artifact.         tar.gz
-[-t]        <tagh>    Output path of compressed artifacts.     Current Directory
-[-r]        <registaryh>    Output path of compressed artifacts.     Current Directory
+[-m]        <mode>          build-deploy-template                       Current Branch
+[-n]        <name>          Compress format of the artifact.            tar.gz
+[-t]        <tagh>          Output path of compressed artifacts.        Current Directory
+[-r]        <registaryh>    Output path of compressed artifacts.        Current Directory
 "
 
-while getopts m:n:t:r:c:e:p:a flag
+while getopts m:n:t:r:c:e:p:a: flag
 do
     case "${flag}" in
         m) mode=${OPTARG};;
@@ -22,15 +32,30 @@ do
 
     esac
 done
-if [ $mode == "build" ];then
+
+if [ -z "$name" ] || [ -z "$tag" ]; then 
+echo "name or tag cant be null "
+exit 0
+fi
+
+if [ "$mode" = "build" ]; then
 docker build -t ${name}:${tag} .
 echo $mode
-elif [ $mode == "deploy" ];then
-echo "zsdasds"
+elif [ "$mode" = "deploy" ];then
 # todo cpu dinamik olarak alınlaı -p yazarak cpu datası alınamıyor
-docker run -m $memory --cpus 1 ${name}:${tag} 
-echo $cpu
-elif [ $mode == "template" ];then
+# null check yapıyoruz alt satırda memory ve cpu verilmemişse limitleri vermeden çalıştırıyoruz ikisi birlikte verilmişse 
+# 2 limitide set ederek çalıştırıyoruz
+if [ -z "$memory" ] && [ -z "$cpu" ]; then 
+docker run ${name}:${tag}
+else
+docker run -m $memory --cpus $cpu ${name}:${tag} 
+fi
+if [ -z "$registary" ];then 
+echo ""
+else
+docker push ${name}:${tag}
+fi
+elif [ "$mode" = "template" ];then
 echo $applicationname
 # todo cpu dinamik olarak alınlaı -p yazarak cpu datası alınamıyor
 docker-compose -f docker-compose-${applicationname} up
---
+++ HomeworkDocker/asd.sh
@@ -0,0 +1,3 @@
+if [ 2 == 32 ];then
+echo "asdasdasd"
+fi
